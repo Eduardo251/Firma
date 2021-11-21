ae34aa501fc66fa4ab8fe5189e3979c64e46b0c0
Eduardo251 <91290449+Eduardo251@users.noreply.github.com>
2021-11-20 23:31:02 -0600

    Firma

---
+++ Firma/Models/ModelFirmas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace Firma.Models
+{
+    public class ModelFirmas
+    {
+        internal object b;
+
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+
+        [MaxLength(70)]
+        public string nombre { get; set; }
+
+        [MaxLength(70)]
+        public string descripcion { get; set; }
+
+        public byte[] ifirma { get; set; }
+
+    }
+}
---
+++ Firma/Views/Page1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Firma.Models;
+
+namespace Firma.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+
+         private void insertdata()
+         {
+             var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PM02.db3");
+             using (var con = new SQLiteConnection(path))
+             {
+                 Image image = new Image();
+                 image.Content = ConvertStreamtoByte();
+                 var result = con.Insert(image);
+             }
+         }
+
+         private object ConvertStreamtoByte()
+         {
+             throw new NotImplementedException();
+         }*/
+
+        private void btnshow_Clicked(object sender, EventArgs e)
+        {
+
+            var image = new Models.ModelFirmas
+            {
+            
+            };
+
+                if (image != null)
+                {
+
+                    byte[] b = image.ifirma;
+                    Stream ms = new MemoryStream(b);
+                    imagen.Source = ImageSource.FromStream(() => ms);
+                }
+
+            
+        }
+    }
+}
